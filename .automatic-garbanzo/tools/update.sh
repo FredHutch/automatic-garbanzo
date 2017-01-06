255f6a7af29e41722e1f569c609238bf306b044c
Michael Gutteridge <mrg@fredhutch.org>
2017-01-06 09:46:22 -0800

    finished update script

--- .automatic-garbanzo/tools/update.sh
+++ .automatic-garbanzo/tools/update.sh
@@ -8,4 +8,43 @@ then
         exit 1
 fi
 
-curl -L https://github.com/FredHutch/automatic-garbanzo/archive/2.0.2.tar.gz | tar -xzv --strip-components=1 -f -
+# Get latest release URL for download
+
+URL=$(curl -sS https://api.github.com/repos/FredHutch/automatic-garbanzo/releases/latest  |jq -r '.tarball_url')
+RELEASE=$(basename ${URL})
+
+# Stash current changes so we can commit this update
+
+git stash save 'automatic-garbanzo update'
+
+# Download and Extract
+
+# Note- we could just `curl | tar` to extract directly without the tempfile but
+# we'd like to get the files committed as part of the process and thus need the
+# manifest of the tarball.
+
+# There's also a way to do it as a oneliner, but there lies madness
+
+DIR=$(mktemp -d)
+OUTPUT=${DIR}/automatic-garbanzo-${RELEASE}.tgz
+curl -sS -o ${OUTPUT} -L ${URL} 
+tar -xzv --strip-components=1 -f ${OUTPUT}
+
+# Stamp release and commit
+
+echo ${RELEASE} > .automatic-garbanzo/RELEASE
+git add .automatic-garbanzo/RELEASE
+
+manifest=($(tar tf ${OUTPUT}))
+
+for target in "${manifest[@]}"
+do
+        echo git add ${target#*/}
+        git add ${target#*/}
+done
+
+git commit -m "Updated automatic garbanzo to release ${RELEASE}"
+git stash apply stash^{/automatic-garbanzo}
+git stash drop $(git stash list |awk -F: '/automatic-garbanzo/{print $1}')
+
+echo "Update complete for this branch"
